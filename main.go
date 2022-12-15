4ee90bcfc9b9568e2238b687dd0e500a9576b3eb
Zenghui Shi <zshi@redhat.com>
2022-12-15 14:25:06 +0800

    update

--- main.go
+++ main.go
@@ -17,80 +17,17 @@ type OVNKubernetesPlugin struct {
 
 const (
 	pluginVersion  = "0.1"
+	pluginName     = "ovn-kubernetes"
 	ConfigFileName = "ovn.yaml"
 )
 
 var Plugin OVNKubernetesPlugin
 
-type OVNKubernetesConfig struct {
-	// Configuration for microshift-ovs-init.service
-	OVSInit OVSInit `json:"ovsInit,omitempty"`
-	// MTU to use for the geneve tunnel interface.
-	// This must be 100 bytes smaller than the uplink mtu.
-	// Default is 1400.
-	MTU uint32 `json:"mtu,omitempty"`
-}
-
-type OVSInit struct {
-	// disable microshift-ovs-init.service.
-	// OVS bridge "br-ex" needs to be configured manually when disableOVSInit is true.
-	DisableOVSInit bool `json:"disableOVSInit,omitempty"`
-	// Uplink interface for OVS bridge "br-ex"
-	GatewayInterface string `json:"gatewayInterface,omitempty"`
-	// Uplink interface for OVS bridge "br-ex1"
-	ExternalGatewayInterface string `json:"externalGatewayInterface,omitempty"`
-}
-
 func init() {
 	Plugin = OVNKubernetesPlugin{
-		Name:    "ovn-kubernetes",
+		Name:    pluginName,
 		Version: pluginVersion,
 	}
-}
-
-func (o *OVNKubernetesConfig) ValidateOVSBridge(bridge string) error {
-	_, err := net.InterfaceByName(bridge)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (o *OVNKubernetesConfig) withDefaults() *OVNKubernetesConfig {
-	o.OVSInit.DisableOVSInit = false
-	o.MTU = 1400
-	return o
-}
-
-func newOVNKubernetesConfigFromFile(path string) (*OVNKubernetesConfig, error) {
-	o := new(OVNKubernetesConfig)
-	buf, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	err = yaml.Unmarshal(buf, &o)
-	if err != nil {
-		return nil, fmt.Errorf("parsing OVNKubernetes config: %v", err)
-	}
-	return o, nil
-}
-
-func NewOVNKubernetesConfigFromFileOrDefault(path string) (*OVNKubernetesConfig, error) {
-	if _, err := os.Stat(path); err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			glog.Info("OVNKubernetes config file not found, assuming default values")
-			return new(OVNKubernetesConfig).withDefaults(), nil
-		}
-		return nil, fmt.Errorf("failed to get OVNKubernetes config file: %v", err)
-	}
-
-	o, err := newOVNKubernetesConfigFromFile(path)
-	if err == nil {
-		glog.Info("got OVNKubernetes config from file %q", path)
-		return o, nil
-	}
-	return nil, fmt.Errorf("getting OVNKubernetes config: %v", err)
 }
 
 func (p *OVNKubernetesPlugin) GetName() string {
@@ -150,14 +87,78 @@ func (p *OVNKubernetesPlugin) GetManifests(kind string) []map[string][]string {
 	return manifests
 }
 
+func (p *OVNKubernetesPlugin) GetRenderParams() string {
+	c, err := NewOVNKubernetesConfigFromFileOrDefault("/etc/microshift/ovn.yaml")
+	if err != nil {
+		return string(1400)
+	}
+	return fmt.Sprint(c.MTU)
+}
+
 func (p *OVNKubernetesPlugin) ValidateConfig() bool {
 	return true
 }
 
-func (p *OVNKubernetesPlugin) GetRenderParams() *OVNKubernetesConfig {
-	c, err := NewOVNKubernetesConfigFromFileOrDefault("/etc/microshift/ovn.yaml")
+type OVNKubernetesConfig struct {
+	// Configuration for microshift-ovs-init.service
+	OVSInit OVSInit `json:"ovsInit,omitempty"`
+	// MTU to use for the geneve tunnel interface.
+	// This must be 100 bytes smaller than the uplink mtu.
+	// Default is 1400.
+	MTU uint32 `json:"mtu,omitempty"`
+}
+
+type OVSInit struct {
+	// disable microshift-ovs-init.service.
+	// OVS bridge "br-ex" needs to be configured manually when disableOVSInit is true.
+	DisableOVSInit bool `json:"disableOVSInit,omitempty"`
+	// Uplink interface for OVS bridge "br-ex"
+	GatewayInterface string `json:"gatewayInterface,omitempty"`
+	// Uplink interface for OVS bridge "br-ex1"
+	ExternalGatewayInterface string `json:"externalGatewayInterface,omitempty"`
+}
+
+func (o *OVNKubernetesConfig) ValidateOVSBridge(bridge string) error {
+	_, err := net.InterfaceByName(bridge)
 	if err != nil {
-		return &OVNKubernetesConfig{}
+		return err
 	}
-	return c
+	return nil
+}
+
+func (o *OVNKubernetesConfig) withDefaults() *OVNKubernetesConfig {
+	o.OVSInit.DisableOVSInit = false
+	o.MTU = 1400
+	return o
+}
+
+func newOVNKubernetesConfigFromFile(path string) (*OVNKubernetesConfig, error) {
+	o := new(OVNKubernetesConfig)
+	buf, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	err = yaml.Unmarshal(buf, &o)
+	if err != nil {
+		return nil, fmt.Errorf("parsing OVNKubernetes config: %v", err)
+	}
+	return o, nil
+}
+
+func NewOVNKubernetesConfigFromFileOrDefault(path string) (*OVNKubernetesConfig, error) {
+	if _, err := os.Stat(path); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			glog.Info("OVNKubernetes config file not found, assuming default values")
+			return new(OVNKubernetesConfig).withDefaults(), nil
+		}
+		return nil, fmt.Errorf("failed to get OVNKubernetes config file: %v", err)
+	}
+
+	o, err := newOVNKubernetesConfigFromFile(path)
+	if err == nil {
+		glog.Info("got OVNKubernetes config from file %q", path)
+		return o, nil
+	}
+	return nil, fmt.Errorf("getting OVNKubernetes config: %v", err)
 }
